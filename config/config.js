6d29dc3319cf2ecfd2bf34086fb5463bbca04948
Leonardo-1998 <leonardo.agst98@gmail.com>
2025-11-07 11:22:44 +0700

    Fix : Mencoba membetukan koneksi database

--- config/config.js
+++ config/config.js
@@ -1,12 +1,19 @@
 import pg from "pg";
 const { Pool } = pg;
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "postgres",
+//   host: "localhost",
+//   port: 5432,
+//   database: "Nutech_Test_DB",
+// });
 
 const pool = new Pool({
-  user: "postgres",
-  password: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "Nutech_Test_DB",
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
